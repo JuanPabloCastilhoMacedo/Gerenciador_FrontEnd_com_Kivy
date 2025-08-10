2584776262073f31e1f58caeb83450ac81f7edb9
Juan <702juanpablo2019@gmail.com>
2025-08-10 18:18:43 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,44 +1,48 @@
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivymd.uix.snackbar import MDSnackbar
 import requests
 
 Builder.load_file("login.kv")
 Builder.load_file("register.kv")
+Builder.load_file("forgot_password.kv")
+
 
 API_URL = "https://gerenciador-com-flask.onrender.com"
 
 class LoginScreen(Screen):
     def login(self):
-        email = self.ids.email.text
-        senha = self.ids.senha.text
+        email = self.ids.email_field.text
+        senha = self.ids.password_field.text
 
         try:
             res = requests.post(f"{API_URL}/login", json={"email": email, "senha": senha})
             data = res.json()
             if data.get("Status") == "OK":
-                print("Login OK:", data.get("mensagem"))
+                print("Login OK:", data.get("message"))
                 self.manager.current = "tasks"
             else:
-                print("Erro:", data.get("mensagem"))
+                print("Erro:", data.get("message"))
         except Exception as e:
             print("Erro ao conectar API:", e)
 
 class RegisterScreen(Screen):
     def register(self):
-        email = self.ids.email.text
-        senha = self.ids.senha.text
+        email = self.ids.email_field.text
+        senha = self.ids.password_field.text
 
         try:
             res = requests.post(f"{API_URL}/register", json={"email": email, "senha": senha})
             data = res.json()
-            if data.get("Status") == "OK":
-                print("Registro OK:", data.get("mensagem"))
+            if res.status_code == 201:
+                print("Registro OK:", data.get("message"))
                 self.manager.current = "login"
             else:
-                print("Erro:", data.get("mensagem"))
+                print("Erro:", data.get("message"))
         except Exception as e:
             print("Erro ao conectar API:", e)
+
 
 def login(self):
     email = self.ids.email.text
@@ -52,13 +56,37 @@ def login(self):
 class TaskScreen(Screen):
     pass  # Depois a gente implementa
 
-class GerenciadorApp(App):
+class ForgotPasswordScreen(Screen):
+    pass
+
+class GerenciadorApp(MDApp):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(LoginScreen(name="login"))
         sm.add_widget(RegisterScreen(name="register"))
+        sm.add_widget(ForgotPasswordScreen(name="forgot_password"))
         sm.add_widget(TaskScreen(name="tasks"))
         return sm
+
+    def send_reset_email(self):
+        email = self.root.get_screen('forgot_password').ids.email_field.text
+        if not email:
+            snack = MDSnackbar()
+            snack.text = "Por favor, insira um e-mail"
+            snack.open()
+            return
+        
+        url = 'https://gerenciador-com-flask.onrender.com/request-password-reset'
+        response = requests.post(url, json={'email': email})
+        if response.status_code == 200:
+            snack = MDSnackbar()
+            snack.text = "E-mail de recuperação enviado"
+            snack.open()
+            self.root.current = 'login'
+        else:
+            snack = MDSnackbar()
+            snack.text = "Erro ao enviar e-mail"
+            snack.open()
 
 if __name__ == "__main__":
     GerenciadorApp().run()
